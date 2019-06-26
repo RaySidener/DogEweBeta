400906d17b2384a6dd044b3f1ca7ea5e88954798
RaySidener <rmssidener@gmail.com>
2019-06-26 10:29:31 -0400

    it compiles now :sunny:

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include "solver.cpp"
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+#include <utility>
+
+using namespace std;
+
+int main(){
+  cout<<"hello math";
+  add(0,2);
+  return 0;
+}
---
+++ solver.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+
+//#include "solver.h"
+using namespace std;
+
+//Solver::Solver(){}
+
+int add(int a, int b){
+  cout<<"we out here";
+  return a+b;
+}
